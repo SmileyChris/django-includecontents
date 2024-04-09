40f149d66b701429deb3904e78e4abe714c0cd3e
Chris Beaven <smileychris@gmail.com>
2024-04-09 16:51:53 +1200

    Move tests around

--- tests/test_multiline.py
+++ tests/test_multiline.py
@@ -2,4 +2,4 @@ from django.template.loader import render_to_string
 
 
 def test_basic():
-    assert render_to_string("multiline.html") == "anon"
+    assert render_to_string("multiline.html") == "anon\n"
