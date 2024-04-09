104aadf1ebe4005817fc8e41e5719a21a260cf9b
Chris Beaven <smileychris@gmail.com>
2024-04-09 16:00:58 +1200

    Use custom loaders so the template doesn't need to be built twice

--- includecontents/engine.py
+++ includecontents/engine.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import django.template.base
 import django.template.engine
+from django.core.exceptions import ImproperlyConfigured
 
 from .base import Template
 
@@ -9,22 +10,40 @@ from .base import Template
 class Engine(django.template.Engine):
     app_dirname = Path("templates") / "components"
 
+    def __init__(
+        self,
+        dirs=None,
+        app_dirs=False,
+        context_processors=None,
+        debug=False,
+        loaders=None,
+        *args,
+        **kwargs,
+    ):
+        if loaders is None:
+            loaders = ["includecontents.loaders.FilesystemLoader"]
+            if app_dirs:
+                loaders += ["includecontents.loaders.AppDirectoriesLoader"]
+            loaders = [("includecontents.loaders.CachedLoader", loaders)]
+        else:
+            if app_dirs:
+                raise ImproperlyConfigured(
+                    "app_dirs must not be set when loaders is defined."
+                )
+        super().__init__(
+            dirs=dirs,
+            app_dirs=False,
+            context_processors=context_processors,
+            debug=debug,
+            loaders=loaders,
+            *args,
+            **kwargs,
+        )
+        self.app_dirs = app_dirs
+
     def from_string(self, template_code):
         """
         Return a compiled Template object for the given template code,
         handling template inheritance recursively.
         """
         return Template(template_code, engine=self)
-
-    def get_template(self, template_name):
-        """
-        Return a compiled Template object for the given template name,
-        handling template inheritance recursively.
-        """
-        template, origin = self.find_template(template_name)
-        if isinstance(template, django.template.base.Template):
-            return Template(template.source, origin, template_name, engine=self)
-        if not hasattr(template, "render"):
-            # template needs to be compiled
-            template = Template(template, origin, template_name, engine=self)
-        return template
---
+++ includecontents/loaders.py
@@ -0,0 +1,50 @@
+import django.template.loaders.app_directories
+import django.template.loaders.base
+import django.template.loaders.cached
+import django.template.loaders.filesystem
+from django.template import TemplateDoesNotExist
+
+from .base import Template
+
+
+class CustomTemplateMixin(django.template.loaders.base.Loader):
+
+    def get_template(self, template_name, skip=None):
+        """
+        Call self.get_template_sources() and return a Template object for
+        the first template matching template_name. If skip is provided, ignore
+        template origins in skip. This is used to avoid recursion during
+        template extending.
+        """
+        tried = []
+
+        for origin in self.get_template_sources(template_name):
+            if skip is not None and origin in skip:
+                tried.append((origin, "Skipped to avoid recursion"))
+                continue
+
+            try:
+                contents = self.get_contents(origin)
+            except TemplateDoesNotExist:
+                tried.append((origin, "Source does not exist"))
+                continue
+            else:
+                return Template(
+                    contents,
+                    origin,
+                    origin.template_name,
+                    self.engine,
+                )
+
+
+class FilesystemLoader(
+    CustomTemplateMixin, django.template.loaders.filesystem.Loader
+): ...
+
+
+class AppDirectoriesLoader(
+    CustomTemplateMixin, django.template.loaders.app_directories.Loader
+): ...
+
+
+class CachedLoader(CustomTemplateMixin, django.template.loaders.cached.Loader): ...
