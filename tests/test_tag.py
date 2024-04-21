8435a60a7e1c3873a5132c063329a9f71edc0d0d
Chris Beaven <smileychris@gmail.com>
2024-04-22 11:15:25 +1200

    Fix tag tests

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -5,10 +5,9 @@ def test_basic():
     assert render_to_string("test_tag/basic.html") == (
         """<div class="outer">
   <div class="inner" id="">
-  
     inner content
-  
-</div>
+  </div>
+
 </div>
 """
     )
@@ -18,10 +17,9 @@ def test_with_attr():
     assert render_to_string("test_tag/with_attr.html", {"inner_id": "hello"}) == (
         """<div class="outer">
   <div class="inner" id="hello">
-  
     inner content
-  
-</div>
+  </div>
+
 </div>
 """
     )
